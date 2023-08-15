6beb482ad1c4a7de236dc189687a31663b875267
Zhaniya110 <zmeyramova@gmail.com>
2023-08-15 18:11:47 +0600

    Commit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '5d51c108994297dd038a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '669df96923ff1796f25a');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import { registerBlockType} from '@wordpress/blocks';
+import { RichText} from '@wordpress/block-editor';
+import block from './block.json';
+import {__} from '@wordpress/i18n';
 
-registerBlockType('udemy-plus/fancy-header',{
-    edit(){
-        return <p>Hello!</p>
+registerBlockType(
+    block.name, {
+        edit({attributes, setAttributes}){
+            const {content} = attributes;
+            return (<RichText tagName="h2" placeholder={__('Enter heading','udemy-plus')}
+            value= {content}
+            onChange = {newVal => setAttributes({content: newVal})}
+            
+            
+            />)
+          
+            
+        },
+        save({attributes}){
+            const {content } = attributes
+            return <RichText.Content
+            tagName="h2"
+            value={content}/>
+                        }
     }
-})
+)
